1b532700530bde0abeafc2fa991bcc132a32f58b
Hyobin (Justin) You <hyobinyou@gmail.com>
2017-07-12 18:48:34 -0400

    Update xlsxToJson.py

--- HRI_handle/xlsxToJson.py
+++ HRI_handle/xlsxToJson.py
@@ -3,43 +3,40 @@ import random
 import csv
 import GetDataFromURL as get
 import json
-import xlrd
 
 def readData(reader):
     allEntries = []
     for row in reader:
-        allEntries+=[(row['RANK_CD'], row['BUSINESS_LAST_NAME'])]
-#        allEntries+=[row]
-    print allEntries
-#    print allEntries[0].
+        allEntries+=[row]
     return allEntries
 
 def main():
-    """ For actual data:
-    focus on the columns of interest
-    recreate each of the "objects" under an array
-    create the json "txt"
-        -go through one person
-            -check if they are supervisor for each entry
-            -if so, then put such "objects" under this person's "children"
-                -check the current growing text file to see if this "object" already is added
-    convert final string txt to json file (?)
-
-    """
-
-    #open reader
+    # open reader
     in_file = open('hri.csv', "rb")
     reader = csv.DictReader(in_file)
     allData = readData(reader)
 
-    #open writer
+    # open writer
     out_file = open('new.json', "w")
-    json.dump("asdf", out_file)
+    json.dump(allData, out_file)
 
-
-    #close out the files
+    # close out the files
     out_file.close()
     in_file.close()
 
+"""creating actual JSON format"""
+def main2():
+        in_file = open('hri.csv', "rb")
+        reader = csv.DictReader(in_file)
 
-main()
+        out_file = open('new.json', 'w')
+
+        for row in reader:
+            json.dump(row, out_file)
+            out_file.write("\n")
+
+        out_file.close()
+
+
+#main()
+main2()
